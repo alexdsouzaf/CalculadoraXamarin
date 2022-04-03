9c129bc69b0502552ef76c34350afa2bf902a048
alexdsouzaf <81589945+alexdsouzaf@users.noreply.github.com>
2022-04-03 13:31:51 -0300

    Projeto funcionando
    No momento desse commit , a calculadora está funcionando mas ainda assim sujeito a refatoração um dia quem sabe

---
+++ Calculadora.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CalculadoraXamarin
+{
+    public partial class Calculadora
+    {
+        #region[ATRIBUTOS]
+        public decimal num1 { get; set; }
+        public decimal num2 { get; set; }
+        public string operador { get; set; }
+        public bool flag { get; set; } = true;
+        #endregion
+
+        #region[CONSTRUTOR]
+        public Calculadora()
+        {
+            this.Eventos();
+            this.InitializeComponents();
+        }
+        #endregion
+
+        #region[EVENTOS]
+        private void Btn0_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "0";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "1";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn2_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "2"; 
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn3_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "3";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn4_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "4";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn5_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "5";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn6_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "6";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn7_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "7";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void Btn8_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "8";
+            AtribuirVariaveisCalculo();
+        }
+        private void Btn9_Clicked(object sender, EventArgs e)
+        {
+            lblVisorNumAtual.Text += "9";
+            AtribuirVariaveisCalculo();
+        }
+
+        private void btnIgual_Clicked(object sender, EventArgs e)
+        {
+            switch (operador)
+            {
+                case "+":
+                    lblVisorNumAtual.Text = Somar();
+                    break;
+                case "-":
+                    lblVisorNumAtual.Text = Subtrair();
+                    break;
+                case "*":
+                    lblVisorNumAtual.Text = Multiplicar();
+                    break;
+                case "/":
+                    lblVisorNumAtual.Text = Dividir();
+                    break;
+                default:
+                    break;
+            }
+
+            num1 = Convert.ToDecimal(lblVisorNumAtual.Text);
+            lblVisorOperacao.Text = string.Empty;
+        }
+        private void btnAdicao_Clicked(object sender, EventArgs e)
+        {
+            operador = "+";
+            ManipulaVisor();
+            lblVisorOperacao.Text += " + ";
+
+        }
+        private void btnSubstracao_Clicked(object sender, EventArgs e)
+        {
+            operador = "-";
+            ManipulaVisor();
+            lblVisorOperacao.Text += " - ";
+        }
+
+        private void btnMultiplicacao_Clicked(object sender, EventArgs e)
+        {
+            operador = "*";
+            ManipulaVisor();
+            lblVisorOperacao.Text += " * ";
+        }
+        private void btnDivisao_Clicked(object sender, EventArgs e)
+        {
+            operador = "/";
+            ManipulaVisor();
+            lblVisorOperacao.Text += " / ";
+        }
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+            flag = true;
+            lblVisorOperacao.Text = string.Empty;
+            lblVisorNumAtual.Text = string.Empty;
+        }
+        #endregion
+
+        #region[METODOS]
+        private string Somar() => (num1 + num2).ToString();
+
+        private string Subtrair() => (num1 - num2).ToString();
+
+        private string Dividir() => (num1 / num2).ToString();
+
+        private string Multiplicar() => (num1 * num2).ToString();
+
+        private void AtribuirVariaveisCalculo()
+        {
+            if (flag)
+                num1 = Convert.ToDecimal(lblVisorNumAtual.Text);
+            else
+                num2 = Convert.ToDecimal(lblVisorNumAtual.Text);
+        }
+
+        private void ManipulaVisor()
+        {
+            lblVisorOperacao.Text += lblVisorNumAtual.Text;
+            lblVisorNumAtual.Text = string.Empty;
+            flag = false;
+        }
+
+        private void Eventos()
+        {
+            btn0.Clicked += Btn0_Clicked;
+            btn1.Clicked += Btn1_Clicked;
+            btn2.Clicked += Btn2_Clicked;
+            btn3.Clicked += Btn3_Clicked;
+            btn4.Clicked += Btn4_Clicked;
+            btn5.Clicked += Btn5_Clicked;
+            btn6.Clicked += Btn6_Clicked;
+            btn7.Clicked += Btn7_Clicked;
+            btn8.Clicked += Btn8_Clicked;
+            btn9.Clicked += Btn9_Clicked;
+            btnIgual.Clicked += btnIgual_Clicked;
+            btnAdicao.Clicked += btnAdicao_Clicked;
+            btnSubstracao.Clicked += btnSubstracao_Clicked;
+            btnMultiplicacao.Clicked += btnMultiplicacao_Clicked;
+            btnDivisao.Clicked += btnDivisao_Clicked;
+            btnClear.Clicked += btnClear_Clicked;
+        }
+        #endregion
+    }
+}
---
+++ Calculadora.layout.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CalculadoraXamarin
+{
+    public partial class Calculadora : ContentPage
+    {
+        private static double TamanhoBotaoX { get; set; } = 1;
+        private static double TamanhoBotaoY { get; set; } = 1.2;
+        #region[COMPONENTES]
+        //numeros
+        Button btn0 = new Button() { Text = "0", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY }; 
+        Button btn1 = new Button() { Text = "1", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn2 = new Button() { Text = "2", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn3 = new Button() { Text = "3", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn4 = new Button() { Text = "4", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn5 = new Button() { Text = "5", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn6 = new Button() { Text = "6", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn7 = new Button() { Text = "7", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn8 = new Button() { Text = "8", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btn9 = new Button() { Text = "9", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        
+        //operadores
+        Button btnAdicao = new Button() { Text = "+", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btnSubstracao = new Button() { Text = "-", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btnMultiplicacao = new Button() { Text = "*", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btnDivisao = new Button() { Text = "/", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+        Button btnIgual = new Button() { Text = "=", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+
+        //recursos
+        Button btnClear = new Button() { Text = "Clear", ScaleX = TamanhoBotaoX, ScaleY = TamanhoBotaoY };
+
+        /// <summary>
+        /// Numero escolhio no momento do click do botão
+        /// </summary>
+        Label lblVisorNumAtual = new Label() { Text = "", FontSize = 44, HorizontalTextAlignment = TextAlignment.End, Margin = 10, Padding = 15 };
+        
+        /// <summary>
+        /// Expressão completa que será calculada
+        /// </summary>
+        Label lblVisorOperacao = new Label() { Text = "", FontSize = 60, HorizontalTextAlignment = TextAlignment.End, Margin = 10, Padding = 15 };
+
+        #endregion
+
+        #region[LAYOUT DA PAGINA]
+        public void InitializeComponents()
+        {
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    lblVisorOperacao,
+                    lblVisorNumAtual,                                
+                    new FlexLayout
+                    {
+                        Margin = new Thickness(20,0),                        
+                        //Padding = new Thickness(10,0),
+                        Children =
+                        {
+                            btn7,
+                            btn8,
+                            btn9,
+                            btnAdicao
+                        }
+                    },                                
+                    new FlexLayout
+                    {
+                        Margin = new Thickness(20,0),
+                        //Padding = new Thickness(10,0),
+                        Children =
+                        {
+                            btn4,
+                            btn5,
+                            btn6,
+                            btnSubstracao
+                        }
+                    },                                
+                    new FlexLayout
+                    {
+                        Margin = new Thickness(20,0),
+                        //Padding = new Thickness(10,0),
+                        Children =
+                        {
+                            btn1,
+                            btn2,
+                            btn3,
+                            btnMultiplicacao
+                        }
+                    },                                
+                    new FlexLayout
+                    {
+                        Margin = new Thickness(20,0),
+                        //Padding = new Thickness(10,0),
+                        Children =
+                        {
+                            btnClear,
+                            btn0,
+                            btnIgual,
+                            btnDivisao
+                        }
+                    },
+        
+                    
+                }
+            };
+        }
+        #endregion
+    }
+}
